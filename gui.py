d43e80232aae760a2840f3ebeef6302573620f7a
Hemant Patel <areakingpanna@gmail.com>
2022-11-13 10:17:28 +0530

    Adding some features

--- gui.py
+++ gui.py
@@ -1,27 +1,31 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
 
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
 sg.theme("black")
-clock = sg.Text("",key="clock")
+clock = sg.Text("", key="clock")
 label = sg.Text("Type in a To Do ")
-input_box = sg.InputText(tooltip="Enter To Do",key="To do")
+input_box = sg.InputText(tooltip="Enter To Do", key="To do")
 list_box = sg.Listbox(values=functions.to_todos(),
                       key='todos',
                       enable_events=True,
-                      size=[45,10])
-edit_button = sg.Button("Edit",button_color="orange")
-add_button = sg.Button("Add",button_color="yellow")
-complete_button = sg.Button("Complete",button_color="green")
-exit_button = sg.Button("Exit",button_color="red")
+                      size=[45, 10])
+edit_button = sg.Button("Edit", button_color="orange")
+add_button = sg.Button("Add", button_color="yellow")
+complete_button = sg.Button("Complete", button_color="green")
+exit_button = sg.Button("Exit", button_color="red")
 window = sg.Window("My To Do App",
                    layout=[[clock],
                            [label],
-                           [input_box,add_button],
-                           [list_box,edit_button,complete_button],[exit_button]],
-                   font=('Helvetica',20))
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button], [exit_button]],
+                   font=('Helvetica', 20))
 while True:
-    event , values = window.read(timeout=200)
+    event, values = window.read(timeout=200)
     window['clock'].update(value=time.strftime("%b %d,%Y %H:%M:%S"))
 
     match event:
@@ -42,7 +46,7 @@ while True:
                 functions.write_todos(todos)
                 window['todos'].update(todos)
             except IndexError:
-                sg.popup("select the item first in the list ",font=('Helvetica',20))
+                sg.popup("select the item first in the list ", font=('Helvetica', 20))
 
         case "Complete":
             try:
@@ -54,7 +58,6 @@ while True:
                 window['To do'].update(value='')
             except IndexError:
                 sg.popup("select the item first in the list ", font=('Helvetica', 20))
-
 
         case "todos":
             window['To do'].update(value=values['todos'][0])
