7cd3df2f0c46ca520d39b419c748df9aaeba43fa
Hemant Patel <areakingpanna@gmail.com>
2022-11-14 14:33:32 +0530

    Implementing web application

--- web.py
+++ web.py
@@ -2,11 +2,27 @@ import streamlit as st
 import functions
 
 todos = functions.to_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my app")
 st.write("It's help to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label="",placeholder="Add new todo......")
+st.text_input(label="",
+              placeholder="Add new todo......",
+              on_change=add_todo,
+              key="new_todo")
